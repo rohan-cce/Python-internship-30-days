98f9a547fa04296b70fa93078688c6de6df31768
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-11-27 12:50:18 +0530

    Add files via upload

---
+++ day5/task2.py
@@ -0,0 +1,6 @@
+def covid(patient_name,body_temp=98):
+    print("Patient name is ",patient_name,"\nBody temperature = ",body_temp,"°C")
+    print("----------------------------------------------------------------")
+
+covid("Corona_wave1",100)
+covid("corona_wave2")
