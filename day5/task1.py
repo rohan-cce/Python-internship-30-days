8d9cfabf1f1e1727b17c24273ae45a7add8ae429
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-11-27 12:49:33 +0530

    Add files via upload

---
+++ day5/task1.py
@@ -0,0 +1,8 @@
+def func(a,b):
+    print("Addition of two numbers is %d"%(a+b))
+    print("Subtraction of two numbers is %d"%(a-b))
+    print("Division of two numbers is %d"%(a/b))
+    print("Multiplication of two numbers is %d"%(a*b))
+
+
+func(a=10,b=5)
