0c3a5da2e297f4568b97a25d4cbb6d577f8068db
Rohan j <rohan.cce@sece.ac.in>
2020-11-28 12:47:06 +0530

    -

--- day4/task2.py
+++
@@ -1,5 +0,0 @@
-tuple1=('h','e','l','l','o')
-
-print("Tuple before reversing = ",tuple1)
-rev_tuple=tuple1[::-1]
-print("Tuple after reversing =",rev_tuple)
--- day4/task3.py
+++
@@ -1,7 +0,0 @@
-tuple1=('h','e','l','l','o')
-
-print("Before conversion ,",tuple1,"\nType is ",type(tuple1))
-
-list1=list(tuple1)
-
-print("After conversion ,",list1,"\nType is ",type(list1))
