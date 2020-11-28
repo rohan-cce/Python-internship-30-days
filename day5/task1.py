3178bf86621eca077137c628f03a44d045991f71
Rohan j <rohan.cce@sece.ac.in>
2020-11-28 12:47:15 +0530

    -

--- day5/task1.py
+++
@@ -1,8 +0,0 @@
-def func(a,b):
-    print("Addition of two numbers is %d"%(a+b))
-    print("Subtraction of two numbers is %d"%(a-b))
-    print("Division of two numbers is %d"%(a/b))
-    print("Multiplication of two numbers is %d"%(a*b))
-
-
-func(a=10,b=5)
