6a90264cfab553395dffa4ad95c6c07e2088d575
Rohan j <rohan.cce@sece.ac.in>
2020-11-30 10:22:26 +0530

    Create task3.py

---
+++ day 8/task3.py
@@ -0,0 +1,6 @@
+try:
+    num1=int(input("Enter 1st number"))
+    num2=int(input("Enter 2nd number"))
+    print(num1+num3)
+except NameError: 
+    print ("\nNameError: Please Use Numbers Only\n")
