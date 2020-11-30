b4df77ebdd75f03102031f036978904257080195
Rohan j <rohan.cce@sece.ac.in>
2020-11-30 10:49:23 +0530

    Create task4.py

---
+++ day 8/task4.py
@@ -0,0 +1,10 @@
+
+try:
+    n = input("Please enter an integer: ")
+    n = int(n)
+    print("Great, you successfully entered an integer!")
+except ValueError:
+    print("Not a valid integer")
+    
+
+
