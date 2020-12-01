8ca5e8c708cdea4e4c2b304c1c73cb56cc018162
Rohan j <rohan.cce@sece.ac.in>
2020-12-01 10:50:27 +0530

    Update task4.py

--- day 9/task4.py
+++ day 9/task4.py
@@ -1,3 +1,3 @@
 number_list = [12, 18 ,24,27 ,35,45]
-res = list(filter(lambda x: (x % 15 == 0), number_list))
-print("Numbers that are divisible by 15 ",res)
+res = list(filter(lambda x: (x % 9 == 0), number_list))
+print("Numbers that are divisible by 9 ",res)
