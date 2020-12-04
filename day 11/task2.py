62f774548f0603cd3079e7fd6790997b9778fd94
Rohan j <rohan.cce@sece.ac.in>
2020-12-04 16:52:57 +0530

    Update task2.py

--- day 11/task2.py
+++ day 11/task2.py
@@ -1,3 +1,6 @@
-l = ['sat', 'bat', 'cat', 'mat'] 
-test = list(map(list, l)) 
-print(test)
+l1=[]
+for i in range(1,9):
+    l1.append(i)
+l2=['a','b','c','d','e','f','g','h']
+l3=list(zip(l1,l2))
+l3
