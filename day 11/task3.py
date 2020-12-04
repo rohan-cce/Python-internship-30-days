a0a5aa27c15cadffd35300ae5aeade58a7195219
Rohan j <rohan.cce@sece.ac.in>
2020-12-04 16:53:45 +0530

    Update task3.py

--- day 11/task3.py
+++ day 11/task3.py
@@ -1,16 +1,3 @@
-def fun(variable): 
-    letters = ['a', 'e', 'i', 'o', 'u'] 
-    if (variable in letters): 
-        return True
-    else: 
-        return False
-  
-  
-sequence = ['g', 'e', 'e', 'j', 'k', 's', 'p', 'r'] 
-
-
-filtered = filter(fun, sequence) 
-  
-print('The filtered letters are:') 
-for s in filtered: 
-    print(s) 
+l=[2,1,5,6,1,8,0]
+l.sort()
+l
