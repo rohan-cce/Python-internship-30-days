64cd2ca8c3e784d4b48708bb2930b3fae18e93f3
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-12-06 21:58:51 +0530

    Update coffee_machine.py

--- day 13/coffee_machine.py
+++ day 13/coffee_machine.py
@@ -1,86 +1,139 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+class coffee:
+    def __init__(self,water,milk,coffee,money):
+        self.water=water
+        self.milk=milk
+        self.coffee=coffee
+        self.money=money
+        
+    def display_ini(self):
+        print("\n*****The report of the coffee machine *****")
+        print("\n Current resource values are :")
+        print(" ----WATER = ",self.water,"ML\n",
+              "----MILK  = ",self.milk,"ML\n",
+              "----COFFEE  = ",self.coffee,"ML\n",
+              "----MONEY = $",self.money)
+        
+    def cof_val(self):
+        self.cof_w=int(input("\nENTER THE AMOUNT OF WATER YOU WANT TO ADD IN YOUR DRINK(in ml) : "))
+        self.cof_m=int(input("ENTER THE AMOUNT OF MILK YOU WANT TO ADD IN YOUR DRINK(in ml) : "))
+        self.cof_c=int(input("ENTER THE AMOUNT OF COFFEE YOU WANT TO ADD IN YOUR DRINK(in ml) : "))
 
-profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+        if(self.cof_w > self.water):
+            print("Sorry there is not enough water resource !!!!!")
+        if(self.cof_m > self.milk):
+            print("Sorry there is not enough milk resource !!!!!")
+        if(self.cof_c > self.coffee):
+            print("Sorry there is not enough coffee resource !!!!!")
 
+    def set(self):
+        self.check=0
+        if(self.cof_w > self.water or self.cof_m > self.milk or self.cof_c > self.coffee):
+            self.check=1
+            
+        
 
-def is_resource_sufficient(order_ingredients):
-    for item in order_ingredients:
-        if order_ingredients[item] > resources[item]:
-            print(f"​Sorry there is not enough {item}.")
-            return False
-    return True
+    def mon_val(self):
 
+        print("\n ***** THE COST OF YOUR COFFEE IS  $10 *****") 
+        print("\n ENTER THE SUFFICIENT MONEY TO GET YOUR DRINK ")
+        print("\n The acceptable money notations are : \nquarter ($0.25)\ndimes (0.10)\nnickles ($0.05)\npennies ($0.01)")
+        qu=int(input("\nEnter the money in  terms of quarter....(for example, if you have 4 coins of quarter ....enter 4) : "))
+        di=int(input("\nEnter the money in  terms of dimes (IF ENTERED SUFFICIENT AMOUNT PLEASE ENTER 0): "))
+        ni=int(input("\nEnter the money in  terms of nickles (IF ENTERED SUFFICIENT AMOUNT PLEASE ENTER 0): "))
+        pe=int(input("\nEnter the money in  terms of pennies (IF ENTERED SUFFICIENT AMOUNT PLEASE ENTER 0): "))
+        tot=(0.25*qu)+(0.10*di)+(0.05*ni)+(0.01*pe) 
 
-def process_coins():
-    print("Please insert coins.")
-    total = int(input("how many quarters?: ")) * 0.25
-    total += int(input("how many dimes?: ")) * 0.1
-    total += int(input("how many nickles?: ")) * 0.05
-    total += int(input("how many pennies?: ")) * 0.01
-    return total
+        if( tot == 5):
+            self.money = self.money+10
+            if(self.cof_w <= self.water and self.cof_m <= self.milk and self.cof_c <= self.coffee):
+                self.water=self.water - self.cof_w
+                self.milk=self.milk - self.cof_m
+                self.coffee=self.coffee - self.cof_c
+            self.tran="success"
+            
+        elif(tot < 5):
+            print("\nSorry that's not enough money...Please try it again")
+            self.tran="failure"
 
-
-def is_transaction_successful(money_received, drink_cost):
-    if money_received >= drink_cost:
-        change = round(money_received - drink_cost, 2)
-        print(f"Here is ${change} in change.")
-        global profit
-        profit += drink_cost
-        return True
-    else:
-        print("Sorry that's not enough money. Money refunded.")
-        return False
-
-
-def make_coffee(drink_name, order_ingredients):
-    for item in order_ingredients:
-        resources[item] -= order_ingredients[item]
-    print(f"Here is your {drink_name} ☕️. Enjoy!")
+        elif(tot > 5):
+            print("\n Here is your change: "+"{:.2f}".format(tot-5),"$")
+            self.money=self.money+10
+            if(self.cof_w <= self.water and self.cof_m <= self.milk and self.cof_c <= self.coffee):
+                self.water=self.water - self.cof_w
+                self.milk=self.milk - self.cof_m
+                self.coffee=self.coffee - self.cof_c
+            self.tran="success"
 
 
-is_on = True
+    def chec(self):
+        self.msg="no load"
+        if(self.water <=50 or self.milk <= 50 or self.coffee <= 50):
+            self.msg="reload the machine"
+            self.water=self.water+500
+            self.milk=self.milk+500
+            self.coffee=self.coffee+500
+                               
 
-while is_on:
-    choice = input("​What would you like? (espresso/latte/cappuccino): ")
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: ${profit}")
-    else:
-        drink = MENU[choice]
-        if is_resource_sufficient(drink["ingredients"]):
-            payment = process_coins()
-            if is_transaction_successful(payment, drink["cost"]):
-                make_coffee(choice, drink["ingredients"])
+C = coffee(500,500,500,100)
+
+print("*****  WELCOME TO KOFFEE KICKS  ******")
+
+run=1000
+
+print("\nTHE MACHINE WILL SERVE THE CUSTOMERS FOR 1000 TIMES \n\n ENJOY THE COFFEE")
+
+machine=input("\nStatus of the coffee machine (on/off): ")
+
+while(run>=1):
+    while(machine.lower()=='on'):
+        print("---------------------------------------------------------------------------------------------------------------------------------")
+        print("\n***ENTER 1 FOR ESPRESSO \n***ENTER 2 FOR LATTE \n***ENTER 3 FOR CAPPUCCINO")
+        cof_choi=int(input("\n ENTER YOUR CHOICE OF COFFEE : "))
+        C.display_ini()
+
+        if(cof_choi==1):
+            C.cof_val()
+            C.set()
+            if(C.check==1):
+                print("Please enter again")
+            else:
+                C.mon_val()
+                if(C.tran=="success"):
+                    print("\n Here is your ESPRESSO. ENJOY!!!!! ")
+
+        elif(cof_choi==2):
+            C.cof_val()
+            C.set()
+            if(C.check==1):
+                print("Please enter again")
+            else:
+                C.mon_val()
+                if(C.tran=="success"):
+                    print("\n Here is your LATTE. ENJOY!!!!! ")
+
+        elif(cof_choi==3):
+            C.cof_val()
+            C.set()
+            if(C.check==1):
+                print("Please enter again")
+            else:
+                C.mon_val()
+                if(C.tran=="success"):
+                    print("\n Here is your CAPPUCCINO. ENJOY!!!!! ")
+
+        elif(cof_choi>3 or cof_choi==0):
+            print("\n:(  You have choosed wrong choice of coffee....Please try it again  :(")
+
+        C.chec()
+        if(C.msg=="reload the machine"):
+            print("\nONE OF THE RESOURCE IS VERY MUCH LOW IN MACHINE")
+            print( "\nSO THE MACHINE RELOADING ")
+            print("\nTHE MACHINE RELOADED SUCCESSFULLY")
+
+        run=run-1
+        if(run==0):
+            print("\nTHE MACHINE IS UNDER MAINTENANCE \n THE MACHINE IS ENJOYING HIS OWN COFFEE")
+            machine="off"
+
+if(machine.lower()=="off"):
+    print("\nPLEASE TURN ON THE MACHINE TO START THE SERVICE")
